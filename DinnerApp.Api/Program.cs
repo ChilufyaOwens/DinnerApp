6418d1f712f1fa8ac5020a72a18774df6cb81cd6
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2024-02-08 22:52:40 +0200

    Injected files in the program.cs

--- DinnerApp.Api/Program.cs
+++ DinnerApp.Api/Program.cs
@@ -1,7 +1,12 @@
+using DinnerApp.Application;
+using DinnerApp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
     builder.Services.AddControllers();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastruture();
 }
 
 
