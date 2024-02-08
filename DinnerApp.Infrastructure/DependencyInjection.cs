515df68d42fbbef45e4e0a10ee09a18818efe8b5
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2024-02-08 22:16:07 +0200

    Added dependency injection extension classes for each project. This will be used to register dependencies

---
+++ DinnerApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DinnerApp.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastruture(this IServiceCollection services)
+    {
+        return services;
+    }
+}
