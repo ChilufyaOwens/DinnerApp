a30a77146f6423f0e11b1d5ef4d06e7714f768e5
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2024-02-09 22:44:50 +0200

    Added JwtTokenGenerator and stored the secret in the user secret

--- DinnerApp.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ DinnerApp.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -2,17 +2,22 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using DinnerApp.Application.Common.Interfaces.Authentication;
+using DinnerApp.Application.Common.Interfaces.Services;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DinnerApp.Infrastructure.Authentication;
 
-public class JwtTokenGenerator : IJwtTokenGenerator
+public class JwtTokenGenerator(IDateTimeProvider timeProvider, IOptions<JwtSettings> jwtSettingsOption) : IJwtTokenGenerator
 {
+    private readonly IDateTimeProvider _timeProvider = timeProvider;
+    private readonly JwtSettings _jwtSettings = jwtSettingsOption.Value;
+
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
         var signedCredentials = new SigningCredentials(
             new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes("super-secret-key")),
+                Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
             SecurityAlgorithms.HmacSha256
         );
         var claims = new[]
@@ -24,8 +29,9 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         };
 
         var securityToken = new JwtSecurityToken(
-            issuer: "OwnatDinner",
-            expires: DateTime.UtcNow.AddDays(1),
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: _timeProvider.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes) ,
             claims: claims,
             signingCredentials: signedCredentials);
 
