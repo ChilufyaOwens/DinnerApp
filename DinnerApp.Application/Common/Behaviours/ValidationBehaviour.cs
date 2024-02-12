925881014bf03479c76cca38fb7122dc1696b505
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2024-02-12 18:24:37 +0200

    Added a generic ValidationBehaviour.cs for validation models

---
+++ DinnerApp.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,30 @@
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace DinnerApp.Application.Common.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse>(IValidator<TRequest>? validator)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IValidator<TRequest>? _validator = validator;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validator is null) return await next();
+        else
+        {
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+            if (validationResult.IsValid) return await next();
+            
+            var errors = validationResult.Errors.ConvertAll(validationFailure => 
+                Error.Validation(validationFailure.PropertyName, validationFailure.ErrorMessage));
+
+            return (dynamic)errors;
+        }
+        
+    }
+}
--- DinnerApp.Application/DependencyInjection.cs
+++ DinnerApp.Application/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using DinnerApp.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DinnerApp.Application;
 
@@ -7,6 +11,11 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        
         return services;
     }
-}
+} 
